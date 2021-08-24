d9b05ec62904480ac5dd1af871d84d9cce46db1f
Yohannes Getu <nede.evoli@gmail.com>
2021-08-24 16:20:25 +0300

    added show image in image screen

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class CameraScreen(Screen):
         filepath = f"files/{current_time}.png"
         self.ids.camera.export_to_png(filepath)
         self.manager.current = 'image_screen'
+        self.manager.current_scrren.ids.img.source = filepath
 
 
 class ImageScreen(Screen):
